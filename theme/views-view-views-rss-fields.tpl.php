861993fce28730517466e273d020ee319745f3af
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2012-02-05 01:45:16 +0100

    Renamed theme functions and template files: "views_view_views_rss_fields" to "views_view_views_rss" and "views_rss_fields_item" to "views_view_views_rss_fields", updated template suggestions for "views_view_views_rss_fields"

--- theme/views-view-views-rss-fields.tpl.php
+++ theme/views-view-views-rss-fields.tpl.php
@@ -1,8 +1,3 @@
-<?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
-
-<rss version="2.0" <?php print $variables['namespaces']; ?>>
-  <channel>
-    <?php print $variables['channel']; ?>
-    <?php print $variables['rows']; ?>
-  </channel>
-</rss>
+<item>
+  <?php print $variables['row']; ?>
+</item>
