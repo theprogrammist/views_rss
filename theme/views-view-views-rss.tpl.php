5eb93b0c698d1ca0615491ad99c488dbdfc39beb
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2012-04-17 01:26:49 +0200

    - changed way how feed is built: use format_xml_elements() instead of theming - updated all preprocess functions to be compatible with format_xml_elements() element format - removed function views_rss_htmlspecialchars() and all calls to it - this is not done automatically by format_xml_elements() - updated views_rss_rewrite_relative_paths() to handle multiple elements and arrays of subelements - updated element definitions to account for updated/removed preprocess functions - added hook_views_rss_feed_alter() - element values are no longer going through any theme() function - removed theme callbacks: views_view_views_rss_fields and views_view_views_rss_field - removed template files: views-view-views-rss-fields.tpl.php and views-view-views-rss-field.tpl.php

--- theme/views-view-views-rss.tpl.php
+++ theme/views-view-views-rss.tpl.php
@@ -5,9 +5,4 @@
  */
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
-<rss version="2.0" <?php print $variables['namespaces']; ?>>
-  <channel>
-    <?php print $variables['channel']; ?>
-    <?php print $variables['rows']; ?>
-  </channel>
-</rss>
+<?php print $rss_feed; ?>
